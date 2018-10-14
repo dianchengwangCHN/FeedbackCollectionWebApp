8c09cbbf3a62b537c45adcf80f9f4bcab4c207b9
Diancheng Wang <dianchengwangchn@gmail.com>
2018-10-14 16:53:33 -0400

    small changes on UI

--- server/client/src/components/Landing.js
+++ server/client/src/components/Landing.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Landing = () => {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>
+    <div style={{ paddingTop: '112px', textAlign: 'center' }}>
+      <h1 style={{ color: '#ff9999', fontSize: '6rem', fontWeight: '600' }}>
         Emaily!
       </h1>
-      Collect feedback from your users
+      <span style={{ color: '#777', fontSize: '1.5rem' }}>Collect feedback from your users</span>
     </div>
   );
 };
