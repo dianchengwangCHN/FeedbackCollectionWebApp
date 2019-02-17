db1e86c682f6616c03f7ec66a8fa474fa1a80a54
dianchengwangCHN <dianchengwangchn@gmail.com>
2019-02-16 23:56:55 -0500

    Improve UI

--- server/client/src/components/Landing.js
+++ server/client/src/components/Landing.js
@@ -1,17 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import M from "materialize-css";
+import "./Landing.css";
 
-const Landing = () => {
-  return (
-    <div style={{ paddingTop: "112px", textAlign: "center" }}>
-      <h1
-        className='grey-text text-darken-2'
-        style={{ fontSize: "6rem", fontWeight: "600" }}
-      >
-        Emaily!
-      </h1>
-      <h5 className='grey-text'>Collect feedback from your users</h5>
-    </div>
-  );
-};
+class Landing extends Component {
+  componentDidMount() {
+    const slider = document.querySelectorAll(".slider");
+    M.Slider.init(slider, {
+      indicators: false,
+      height: "100%",
+      duration: 500,
+      interval: 6000
+    });
+  }
+  render() {
+    var startButton = null;
+    if (this.props.auth) {
+      startButton = (
+        <Link
+          to='surveys'
+          className='waves-effect waves-light btn white black-text'
+        >
+          Get Started
+        </Link>
+      );
+    }
+    return (
+      <div>
+        <div className='slider fullscreen'>
+          <ul className='slides'>
+            <li>
+              <img src={require("../assets/image/landing1.jpg")} />
+            </li>
+            <li>
+              <img src={require("../assets/image/landing2.jpg")} />
+            </li>
+            <li>
+              <img src={require("../assets/image/landing3.jpg")} />
+            </li>
+          </ul>
+        </div>
+        <div className='float-wrapper'>
+          <h1
+            className='white-text'
+            style={{ fontSize: "6rem", fontWeight: "600" }}
+          >
+            Emaily!
+          </h1>
+          <h5 className='white-text'>Collect feedback from your users</h5>
+          {startButton}
+        </div>
+      </div>
+    );
+  }
+}
 
-export default Landing;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Landing);
